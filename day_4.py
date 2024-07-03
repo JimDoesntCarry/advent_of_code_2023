81a7e1b4c66a52a6d706df2e449b06e8b4aae487
JimDoesntCarry <yazar.strulik@gmail.com>
2024-07-03 09:21:56 +0200

    dunno

--- day_4.py
+++ day_4.py
@@ -1,28 +1,35 @@
 input_file = open("day_4_input.txt", "r")
 data_raw = input_file.read()
 data_matrix = data_raw.split('\n')
-
+data_matrix.pop()
+tickets = []
 punktzahl = 0
-
+cards = 0
+y=0
 for ticket in data_matrix:
-    try:
-        ticket = ticket.split(": ")[1].split(" | ")
-    except Exception as e:
-        print(e)
-        print(ticket)
+    ticket = ticket + " | 0"
+    ticket = ticket.split(": ")[1].split(" | ")
     ticket = [nr.split(" ") for nr in ticket]
-    counter = 0
     for i in range(len(ticket)):
         ticket[i] = list(filter(None, ticket[i]))
-    try:
+    for i in range(len(ticket)):
+        for j in range(len(ticket[i])):
+            ticket[i][j] = int(ticket[i][j])
+    tickets.append(ticket)
+
+for ticket in tickets:
+    y += 1
+    for iteration in range(int(ticket[2][0])+1):
+        cards += 1
+        counter = 0
         for tipp in ticket[1]:
             if tipp in ticket[0]:
                 counter += 1
-    except Exception as e:
-        print(e)
-        print(ticket)
-    if counter == 1:
-        punktzahl += 1
-    elif counter > 1:
-        punktzahl += 2**(counter-1)
-print(punktzahl)
+        for x in range(counter):
+            try:
+                tickets[x+y][2][0] += 1
+            except Exception as e:
+                print(e)
+                print(x,y)
+    
+print(cards)
